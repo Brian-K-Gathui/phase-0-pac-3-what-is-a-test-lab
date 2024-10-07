dad68ea9ab69dc10d826ebe26a1795dc02c996dd
Brian <briankgathui@gmail.com>
2024-10-07 04:54:41 +0300

    completing the lab

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";  // Update to "Susan" as required by the test
+const height = 30;  // Make sure height is less than 40
 const message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message};
+module.exports = { name, height, message };
